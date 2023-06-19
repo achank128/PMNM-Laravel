6d10236653c6c5e3cec0e13d8dc13765d3fd423c
NKChang <khacchang128@gmail.com>
2023-06-19 10:36:11 +0700

    update side bar

--- resources/views/Share/sidebar.blade.php
+++ resources/views/Share/sidebar.blade.php
@@ -8,18 +8,16 @@
     <!-- Sidebar -->
     <div class="sidebar">
         <!-- Sidebar user (optional) -->
-        {{-- <div class="user-panel mt-3 pb-3 mb-3 d-flex align-items-center">
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex align-items-center">
             <div class="image">
                 <img src="/theme/dist/img/user2-160x160.jpg"
                     class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#"
-                    class="d-block"><b>{{ auth()->user()->name }}</b></a>
-                <a href="#"
-                    class="d-block">{{ auth()->user()->email }}</a>
+                <a href="#" class="d-block"><b>Nguyễn Khắc Cháng</b></a>
+                <a href="#" class="d-block">chang21864@huce.edu.vn</a>
             </div>
-        </div> --}}
+        </div>
 
         <!-- SidebarSearch Form -->
         <div class="form-inline">
